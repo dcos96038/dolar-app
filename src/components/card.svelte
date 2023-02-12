ecae5a575e4ba49caa8824a416fc971f9deb3ade
Diego Coscolla <56701155+dcos96038@users.noreply.github.com>
2023-02-12 15:37:53 -0300

    fix card info bugs

--- src/components/card.svelte
+++ src/components/card.svelte
@@ -10,6 +10,21 @@
 		actualizacion: string;
 	}
 	export let info: IInfo;
+
+	console.log(info);
+
+	const parseToCurrency = (value: string) => {
+		const parsedToNumberValue = Number(value.replace('.', '').replace(',', '.'));
+
+		if (isNaN(parsedToNumberValue) || parsedToNumberValue === 0) {
+			return '-';
+		}
+
+		return parsedToNumberValue.toLocaleString('es-AR', {
+			style: 'currency',
+			currency: 'ARS'
+		});
+	};
 </script>
 
 <div
@@ -23,24 +38,31 @@
 	<div class="flex justify-around items-center border-b-2 border-black pb-1">
 		<div class="flex flex-col justify-center items-center">
 			<span class="uppercase font-semibold">compra</span>
-			<span>${info.compra}</span>
+			<span
+				>{parseToCurrency(info.compra)} {info.nombre.toUpperCase() === 'BITCOIN' ? 'USD' : ''}</span
+			>
 		</div>
 		<div class="flex flex-col justify-center items-center">
 			<span class="uppercase font-semibold">venta</span>
-			<span>${info.venta}</span>
+			<span>{parseToCurrency(info.venta)}</span>
 		</div>
 	</div>
 	<div class="border-b-2 border-black pb-1 flex justify-center items-center gap-1	">
 		<span
 			class={`${
 				Number(info.variacion.replace(',', '.')) > 0 ? 'text-red-900' : 'text-green-900 rotate-180'
-			}`}>▲</span
+			}`}
+		>
+			{info.nombre.toUpperCase() === 'BITCOIN' || info.variacion === '0' ? '❖' : '▲'}</span
 		>
 		<span class="font-bold"> Variación: </span>
 		<span
 			class={`${
 				Number(info.variacion.replace(',', '.')) > 0 ? 'text-red-900' : 'text-green-900'
-			} font-semibold`}>{info.variacion}%</span
+			} font-semibold`}
+			>{info.nombre.toUpperCase() === 'BITCOIN' || info.variacion === '0'
+				? '-'
+				: info.variacion + '%'}</span
 		>
 	</div>
 	<div class="uppercase font-semibold flex justify-center items-center">
