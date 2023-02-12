c48c7f503e1e8621a2a59adddf9f2b58596f41fa
Diego Coscolla <56701155+dcos96038@users.noreply.github.com>
2023-02-12 15:17:08 -0300

    add cards container

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,22 @@
-<h1 class="text-3xl font-bold underline">Hello world!</h1>
+<script lang="ts">
+	import Card from '../components/card.svelte';
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
+<div class="pt-6 flex flex-col gap-20">
+	<h1 class="text-5xl font-bold uppercase text-center">cotizaciones</h1>
+	<div class="flex justify-center flex-wrap gap-10 items-stretch mx-auto mb-10">
+		{#if data.dolarInfo}
+			{#each data.dolarInfo as cotizacionData}
+				{#if cotizacionData.variacion}
+					<Card info={cotizacionData} />
+				{/if}
+			{/each}
+		{/if}
+	</div>
+</div>
 
 <style lang="postcss">
 	:global(html) {
